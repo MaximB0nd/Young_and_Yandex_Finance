d19b87b2ad5df8cadde7044554f30e1268f8a094
MaximB0nd <sersim11@yandex.ru>
2025-06-12 13:11:08 +0300

    Init commit

---
+++ Young-and-Yandex_Finance/Application/Young_and_Yandex_FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Young_and_Yandex_FinanceApp.swift
+//  Young-and-Yandex_Finance
+//
+//  Created by Максим Бондарев on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Young_and_Yandex_FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+           
+        }
+    }
+}
---
+++ Young-and-Yandex_FinanceTests/Young_and_Yandex_FinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Young_and_Yandex_FinanceTests.swift
+//  Young-and-Yandex_FinanceTests
+//
+//  Created by Максим Бондарев on 12.06.2025.
+//
+
+import Testing
+@testable import Young_and_Yandex_Finance
+
+struct Young_and_Yandex_FinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
