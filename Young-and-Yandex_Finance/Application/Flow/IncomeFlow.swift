772988cab77b56b98216b2fd5eff8cc4db3bb989
MaximB0nd <sersim11@yandex.ru>
2025-06-22 22:25:30 +0300

    PlusButton, color refactoring, some changes

--- Young-and-Yandex_Finance/Application/Flow/IncomeFlow.swift
+++ Young-and-Yandex_Finance/Application/Flow/IncomeFlow.swift
@@ -12,60 +12,26 @@ struct IncomeFlow: View {
     @ObservedObject var transactionService: TransactionsService
     
     var body: some View {
+        
         NavigationStack {
             IncomeScreen(transactionService: transactionService)
                 .navigationTitle("Доходы сегодня")
                 .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        minusButton
-                    }
-                    ToolbarItem(placement: .topBarLeading) {
-                        plusButton
-                    }
                     ToolbarItem(placement: .topBarTrailing) {
                         NavigationLink {
-                            MyHistoryScreen(direction: .income, transactionService: transactionService)
+                            MyHistoryFlow(direction: .income, transactionService: transactionService)
                                 .navigationTitle("Моя история")
-                                
+                            
                         } label: {
                             Image(systemName: "clock")
-                                .foregroundStyle(Color("Clock"))
+                                .foregroundStyle(.people)
                         }
                     }
                     
                 }
         }
-    }
-    
-    var plusButton: some View {
-        Button("+"){
-            Task{
-                try! await transactionService.createTransaction(
-                account:
-                        .init(
-                            id: 1,
-                            name: "Max",
-                            balance: 1000,
-                            currency: "RUB"),
-                category:
-                        .init(
-                            id: 1,
-                            name: "Inding",
-                            emoji: "🐕",
-                            direction: .income),
-                amount: Decimal(Int.random(in: 1...10000)),
-                transactionDate: Calendar.current.date(byAdding: .day, value: Int.random(in: -10...10), to: .now)!)
-            }
-        }
-    }
-    
-    var minusButton: some View {
-        Button("-"){
-            Task {
-                if let lastTransactionId = transactionService._transactions.first(where: {$0.category.direction == .income})?.id {
-                    try await transactionService.deleteTransaction(id: lastTransactionId)
-                }
-            }
-        }
+        
+        
+        
     }
 }
---
+++ Young-and-Yandex_Finance/Application/Flow/MyHistoryFlow.swift
@@ -0,0 +1,30 @@
+//
+//  MyHistoryFlow.swift
+//  Young-and-Yandex_Finance
+//
+//  Created by Максим Бондарев on 22.06.2025.
+//
+
+import SwiftUI
+
+struct MyHistoryFlow: View {
+    
+    let direction: Direction
+    @ObservedObject var transactionService: TransactionsService
+    
+    var body: some View {
+        MyHistoryScreen(direction: direction, transactionService: transactionService)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        
+                    } label: {
+                        Image(systemName: "document")
+                    }.tint(.people)
+                }
+            }
+            .onAppear {
+                UIBarButtonItem.appearance().tintColor = .people
+            }
+    }
+}
