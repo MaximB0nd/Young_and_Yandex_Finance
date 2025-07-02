fb7bd303db858ebe19a775de04e3467443d57251
MaximB0nd <sersim11@yandex.ru>
2025-07-02 20:22:25 +0300

    BankAccountsService

--- Young-and-Yandex_Finance/Application/Services/BankAccountsService.swift
+++ Young-and-Yandex_Finance/Application/Services/BankAccountsService.swift
@@ -29,7 +29,7 @@ final class BankAccountsService {
                     id: i,
                     userId: i,
                     name: "Max",
-                    balance: Decimal(i*100),
+                    balance: Decimal(0),
                     currency: i % 2 == 0 ? "USD" : "RUB",
                     createdAt: Date.now,
                     updatedAt: Date.now)
